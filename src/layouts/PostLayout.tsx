fba56123465420a5ac399c8463bb9f782e4071e4
Efraim Munthe <efrayanglain@gmail.com>
2024-08-13 22:28:35 +0700

    replicate postLayout

--- src/layouts/PostLayout.tsx
+++ src/layouts/PostLayout.tsx
@@ -1,3 +1,5 @@
+import config from "@config/config.json";
+import { dateFormat } from "@lib/utils/dateFormat";
 import { TinaMarkdown } from "tinacms/dist/rich-text";
 import type { PostQuery, PostQueryVariables } from "tina/__generated__/types";
 import { useTina } from "tinacms/dist/react";
@@ -20,9 +22,55 @@ export default function PostLayout({ tinaProps }: Props) {
   const { post } = data;
 
   return (
-    <main>
-      <h1>{post.title}</h1>
-      <TinaMarkdown content={post.body}></TinaMarkdown>
-    </main>
+    <>
+      <div className="container">
+        <div className="row justify-content-center text-center my-5">
+          <div className="col-lg-10">
+            {post.categories &&
+              post.categories.map((categorie) => (
+                <a
+                  href={`${config.site.baseUrl}categories/${categorie}/`}
+                  className="d-inline-block link-cta mb-4 text-uppercase"
+                >
+                  {categorie}
+                </a>
+              ))}
+            <h1 className="mb-4 text-center">{post.title}</h1>
+            <p className="small mb-5 text-center">
+              <span className="text-uppercase">{dateFormat(post.date)}</span>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-skew bg-skew-light">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-8">
+              <article className="pb-2">
+                <TinaMarkdown content={post.body}></TinaMarkdown>
+
+                <p className="mt-5 text-center">
+                  {post.tags && (
+                    <span className="text-secondary">Tagged: </span>
+                  )}
+                  {post.tags &&
+                    post.tags.map((tag) => (
+                      <a
+                        href={`${config.site.baseUrl}tag/${tag}/`}
+                        className="link-tag text-dark"
+                      >
+                        #{tag}
+                      </a>
+                    ))}
+                </p>
+              </article>
+
+              {/*TOOD: add slugPrev and slugNext */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
